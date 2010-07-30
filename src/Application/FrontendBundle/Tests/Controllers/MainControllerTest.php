5724fff36d4f98164e0d364c43ff50b406650990
ornicar <thibault.duplessis@gmail.com>
2010-07-30 12:01:52 +0200

    More functional tests

--- src/Application/FrontendBundle/Tests/Controllers/MainControllerTest.php
+++ src/Application/FrontendBundle/Tests/Controllers/MainControllerTest.php
@@ -13,4 +13,21 @@ class MainControllerTest extends WebTestCase
         $this->assertTrue($client->getResponse()->isSuccessful());
         $this->assertEquals(1, $crawler->filter('#logo-text:contains("Symfony2 CMF")')->count());
     }
+
+    public function testAboutShowsTableOfSponsors()
+    {
+        $client = $this->createClient();
+        $crawler = $client->request('GET', '/about');
+        $this->assertTrue($client->getResponse()->isSuccessful());
+        $this->assertEquals(1, $crawler->filter('table thead th:contains("Company")')->count());
+    }
+
+    public function testGetInvolvedShowsALinkToGithubWiki()
+    {
+        $client = $this->createClient();
+        $crawler = $client->request('GET', '/get-involved');
+        $this->assertTrue($client->getResponse()->isSuccessful());
+        $this->assertEquals(1, $crawler->filter('a:contains("Github Wiki")')->count());
+    }
+
 }
