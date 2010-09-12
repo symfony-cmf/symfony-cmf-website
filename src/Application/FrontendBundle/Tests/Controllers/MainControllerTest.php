10179effaa1e4ac0983494cdd1c6f269cb31abc3
ornicar <thibault.duplessis@gmail.com>
2010-09-12 12:16:14 -0700

    Improve tests by using route names instead of hardcoded urls

--- src/Application/FrontendBundle/Tests/Controllers/MainControllerTest.php
+++ src/Application/FrontendBundle/Tests/Controllers/MainControllerTest.php
@@ -9,7 +9,7 @@ class MainControllerTest extends WebTestCase
     public function testIndexShowsLogoAndNavigation()
     {
         $client = $this->createClient();
-        $crawler = $client->request('GET', '/');
+        $crawler = $client->request('GET', $client->getContainer()->get('router')->generate('homepage'));
         $this->assertTrue($client->getResponse()->isSuccessful());
         $this->assertEquals(1, $crawler->filter('#logo-text:contains("Symfony2 CMF")')->count());
     }
@@ -17,7 +17,7 @@ class MainControllerTest extends WebTestCase
     public function testAboutShowsTableOfSponsors()
     {
         $client = $this->createClient();
-        $crawler = $client->request('GET', '/about');
+        $crawler = $client->request('GET', $client->getContainer()->get('router')->generate('about'));
         $this->assertTrue($client->getResponse()->isSuccessful());
         $this->assertEquals(1, $crawler->filter('table thead th:contains("Company")')->count());
     }
@@ -25,7 +25,7 @@ class MainControllerTest extends WebTestCase
     public function testGetInvolvedShowsALinkToGithubWiki()
     {
         $client = $this->createClient();
-        $crawler = $client->request('GET', '/get-involved');
+        $crawler = $client->request('GET', $client->getContainer()->get('router')->generate('get_involved'));
         $this->assertTrue($client->getResponse()->isSuccessful());
         $this->assertEquals(1, $crawler->filter('a:contains("Github Wiki")')->count());
     }
@@ -33,7 +33,7 @@ class MainControllerTest extends WebTestCase
     public function testClickSiteTitleGoToHomepage()
     {
         $client = $this->createClient();
-        $crawler = $client->request('GET', '/get-involved');
+        $crawler = $client->request('GET', $client->getContainer()->get('router')->generate('get_involved'));
         $client->click($crawler->selectLink('Symfony2 CMF')->link());
         $this->assertEquals('Application\FrontendBundle\Controller\MainController::indexAction', $client->getRequest()->attributes->get('_controller'));
     }
@@ -42,7 +42,7 @@ class MainControllerTest extends WebTestCase
     {
         $this->markTestSkipped("The current route detection in template does not work during tests. But it works when running the application on a browser");
         $client = $this->createClient();
-        $crawler = $client->request('GET', '/get-involved');
+        $crawler = $client->request('GET', $client->getContainer()->get('router')->generate('get_involved'));
         $this->assertEquals(1, $crawler->filter('#nav li.current a:contains("Get Involved")')->count());
         $this->assertEquals(0, $crawler->filter('#nav li.current a:contains("Home")')->count());
         $this->assertEquals(0, $crawler->filter('#nav li.current a:contains("About")')->count());
