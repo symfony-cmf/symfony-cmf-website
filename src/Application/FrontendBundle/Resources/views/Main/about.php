4bf4cd79465c55795e1df68fbc7a8ffdbab58aa3
Lukas Kahwe Smith <smith@pooteeweet.org>
2011-06-25 12:49:04 +0200

    updated company sponsors (also sorted them by alphabet), updated links, disabled currently irrelevant ones and added <a> tags to the others

--- src/Application/FrontendBundle/Resources/views/Main/about.php
+++ src/Application/FrontendBundle/Resources/views/Main/about.php
@@ -24,14 +24,29 @@ feel invited to join the effort with or without the backing of a company behind 
     </thead>
     <tbody>
         <tr>
-            <td><a href="http://www.thegood.com" title="The Good">theGOOD</a></td>
-            <td>Portland, OR USA</td>
-            <td>Kris Wallsmith</td>
+            <td><a href="http://www.ideato.it" title="Ideato">Ideato</a></td>
+            <td>Cesena, Italy</td>
+            <td>Michele, Manuel, Francesco, Filippo, Jacopo</td>
         </tr>
         <tr>
             <td><a href="http://www.knplabs.com" title="knpLabs">knpLabs</a></td>
             <td>France</td>
-            <td>Florian Klein (docteurklein), Vincent Jousse (vjousse), Antoine Hérault (herzult), Matthieu Bontemps (mbontemps)</td>
+            <td>Florian Klein, Vincent Jousse, Antoine Hérault, Matthieu Bontemps, Ryan Weaver</td>
+        </tr>
+        <tr>
+            <td><a href="http://www.liip.ch" title="Liip AG">Liip AG</a></td>
+            <td>Switzerland</td>
+            <td>Lukas, Ebi, David, Chregu</td>
+        </tr>
+        <tr>
+            <td><a href="http://www.nelm.io" title="nelmio">nelmio</a></td>
+            <td>Switzerland</td>
+            <td>Jordi</td>
+        </tr>
+        <tr>
+            <td><a href="http://www.opensky.com" title="openSky">openSky</a></td>
+            <td>USA</td>
+            <td>Kris Wallsmith, Jonathan H. Wage</td>
         </tr>
         <tr>
             <td><a href="http://pixelfusion.co.nz" title="Pixel Fusion">Pixel Fusion</a></td>
@@ -39,29 +54,14 @@ feel invited to join the effort with or without the backing of a company behind 
             <td>Keri Henare</td>
         </tr>
         <tr>
-            <td><a href="http://www.iostudio.com" title="Iostudio">Iostudio</a></td>
-            <td>Nashville, TN USA</td>
-            <td>Ryan Weaver</td>
-        </tr>
-        <tr>
-            <td><a href="http://www.liip.ch" title="Liip AG">Liip AG</a></td>
-            <td>Switzerland</td>
-            <td>Lukas, Jordi, Ebi, David, Chregu</td>
-        </tr>
-        <tr>
             <td><a href="http://www.sensiolabs.com" title="Sensio Labs">Sensio Labs</a></td>
-            <td>Paris, France | Nashville, TN USA</td>
-            <td>Jonathan H. Wage, Fabien Potencier</td>
+            <td>Paris, France</td>
+            <td>Fabien Potencier</td>
         </tr>
         <tr>
             <td><a href="http://servergrove.com" title="ServerGrove">ServerGrove</a></td>
             <td>Miami, Florida</td>
             <td>Pablo Godel</td>
-        </tr>
-        <tr>
-            <td><a href="http://www.ideato.it" title="Ideato">Ideato</a></td>
-            <td>Cesena, Italy</td>
-            <td>Michele, Manuel, Francesco, Filippo, Jacopo</td>
         </tr>
     </tbody>
 </table>
@@ -69,11 +69,11 @@ feel invited to join the effort with or without the backing of a company behind 
 <h2>Resources/Links</h2>
 
 <ul>
-    <li><strong>GitHub</strong>: https://github.com/symfony-cmf/symfony-cmf</li>
-    <li><strong>Website</strong>: http://cmf.symfony.com/
-    <li><strong>Wiki</strong>: http://wiki.github.com/symfony-cmf/symfony-cmf/</li>
-    <li><strong>Issue Tracker</strong>: http://cmf.symfony.com/redmine/</li>
-    <li><strong>IRC</strong>: #symfony-cmf @ irc.freenode.org</li>
-    <li><strong>Users mailing list</strong>: http://groups.google.com/group/symfony-cmf-users</li>
-    <li><strong>Devs mailing list</strong>: http://groups.google.com/group/symfony-cmf-devs</li>
+    <li><strong>GitHub</strong>: <a href="https://github.com/symfony-cmf/symfony-cmf">https://github.com/symfony-cmf/symfony-cmf</a></li>
+    <li><strong>Website</strong>: <a href="http://cmf.symfony.com">http://cmf.symfony.com</a>
+    <li><strong>Wiki</strong>: <a href="http://wiki.github.com/symfony-cmf/symfony-cmf">http://wiki.github.com/symfony-cmf/symfony-cmf</a></li>
+<!--    <li><strong>Issue Tracker</strong>: http://cmf.symfony.com/redmine/</li> -->
+    <li><strong>IRC</strong>: <a href="irc://freenode/#symfony-cmf">#symfony-cmf @ irc.freenode.org</a></li>
+<!--    <li><strong>Users mailing list</strong>: http://groups.google.com/group/symfony-cmf-users</li> -->
+    <li><strong>Devs mailing list</strong>: <a href="http://groups.google.com/group/symfony-cmf-devs">http://groups.google.com/group/symfony-cmf-devs</a></li>
 </ul>
