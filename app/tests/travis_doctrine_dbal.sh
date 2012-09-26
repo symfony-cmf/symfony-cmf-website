8511a000a92efab1be0a6dfd9517e216b6ce4b95
Lukas Kahwe Smith <smith@pooteeweet.org>
2012-09-26 12:54:35 +0200

    setup travis

--- app/tests/travis_doctrine_dbal.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-php composer.phar require jackalope/jackalope-doctrine-dbal:dev-master
-
-mysql -e 'create database IF NOT EXISTS sandbox;' -u root
-
-php app/console doctrine:phpcr:init:dbal -e=test
