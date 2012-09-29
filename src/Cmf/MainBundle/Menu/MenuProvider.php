84fbd36c8292a24be982516dbf36f7d2d0c5a714
Lukas Kahwe Smith <smith@pooteeweet.org>
2012-09-29 14:50:27 +0200

    added news subsections

--- src/Cmf/MainBundle/Menu/MenuProvider.php
+++ src/Cmf/MainBundle/Menu/MenuProvider.php
@@ -2,27 +2,25 @@
 
 namespace Cmf\MainBundle\Menu;
 
-use Knp\Menu\FactoryInterface;
-use \Knp\Menu\MenuItem;
-use Symfony\Component\DependencyInjection\ContainerAware;
-use Symfony\Component\HttpFoundation\Request;
+use Knp\Menu\MenuItem;
 
-class MenuProvider extends \Symfony\Cmf\Bundle\MenuBundle\Provider\PHPCRMenuProvider
+use Symfony\Cmf\Bundle\MenuBundle\Provider\PHPCRMenuProvider;
+
+class MenuProvider extends PHPCRMenuProvider
 {
     public function get($name, array $options = array())
     {
-       if ('simple' == $name)
-       {
-           //get the internal pages of the website
-           $menu = parent::get($name, $options);
+        //get the internal pages of the website
+        $menu = parent::get($name, $options);
 
-           //Home menu item
-           $item = new MenuItem('Home', $this->factory);
-           $item->setUri($menu->getUri());
-           $menu->addChild($item);
-           $item->moveToFirstPosition();
+        if ($name === 'simple') {
+            //Home menu item
+            $item = new MenuItem('Home', $this->factory);
+            $item->setUri($menu->getUri());
+            $menu->addChild($item);
+            $item->moveToFirstPosition();
+        }
 
-           return $menu;
-       }
+        return $menu;
     }
 }
