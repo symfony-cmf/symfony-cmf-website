a6306ed5e4d6f45fc84adde40568284444c33b80
David Buchmann <david.buchmann@liip.ch>
2012-10-03 13:49:52 +0200

    preparing for news collections and updating create bundle cleanup

---
+++ src/Cmf/MainBundle/Document/NewsCollection.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Cmf\MainBundle\Document;
+
+use Doctrine\ODM\PHPCR\Mapping\Annotations as PHPCRODM;
+
+use Symfony\Component\Validator\Constraints as Assert;
+
+use Symfony\Cmf\Bundle\SimpleCmsBundle\Document\Page;
+
+use Symfony\Cmf\Component\Routing\RouteAwareInterface;
+use Symfony\Cmf\Bundle\RoutingExtraBundle\Document\Route;
+use Symfony\Cmf\Bundle\CoreBundle\PublishWorkflow\PublishWorkflowInterface;
+
+/**
+ * @PHPCRODM\Document
+ */
+class NewsCollection extends Page
+{
+    /**
+     * @PHPCRODM\Children
+     */
+    private $children;
+
+    public function getChildren()
+    {
+        return $this->children;
+    }
+}
