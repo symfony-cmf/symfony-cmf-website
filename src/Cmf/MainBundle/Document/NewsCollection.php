cb3625ab2a1de9c4dad1f7cd3cbdadae24ee9ab2
Adrien Nicolet <adrien.nicolet@gmail.com>
2012-10-11 11:20:22 +0200

    creation of news with CreateJs

--- src/Cmf/MainBundle/Document/NewsCollection.php
+++ src/Cmf/MainBundle/Document/NewsCollection.php
@@ -4,13 +4,7 @@ namespace Cmf\MainBundle\Document;
 
 use Doctrine\ODM\PHPCR\Mapping\Annotations as PHPCRODM;
 
-use Symfony\Component\Validator\Constraints as Assert;
-
 use Symfony\Cmf\Bundle\SimpleCmsBundle\Document\Page;
-
-use Symfony\Cmf\Component\Routing\RouteAwareInterface;
-use Symfony\Cmf\Bundle\RoutingExtraBundle\Document\Route;
-use Symfony\Cmf\Bundle\CoreBundle\PublishWorkflow\PublishWorkflowInterface;
 
 /**
  * @PHPCRODM\Document
