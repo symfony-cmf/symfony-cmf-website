d03ffda8a62ae1501093751a4d4bc96da495a42c
Lukas Kahwe Smith <smith@pooteeweet.org>
2012-10-27 15:43:31 +0200

    cosmetics

--- src/Cmf/MainBundle/DependencyInjection/CmfMainExtension.php
+++ src/Cmf/MainBundle/DependencyInjection/CmfMainExtension.php
@@ -1,19 +1,16 @@
 <?php
+
 namespace Cmf\MainBundle\DependencyInjection;
 
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
-//TODO: make config xml instead of yml
 use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 use Symfony\Component\Config\FileLocator;
-
 
 class CmfMainExtension extends Extension
 {
     public function load(array $configs, ContainerBuilder $container)
     {
-        $config = $this->processConfiguration(new Configuration(), $configs);
-
         $loader = new YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
         $loader->load('services.yml');
     }
