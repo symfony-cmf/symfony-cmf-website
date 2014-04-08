43000b212ee5e7ecb66cf3d34a755fc33c576f26
ElectricMaxxx <Maximilian.Berghoff@gmx.de>
2014-04-08 14:23:28 +0200

    add seo default behavior

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -28,9 +28,12 @@ class AppKernel extends Kernel
             new Symfony\Cmf\Bundle\BlockBundle\CmfBlockBundle(),
             new Symfony\Cmf\Bundle\CoreBundle\CmfCoreBundle(),
             new Symfony\Cmf\Bundle\SimpleCmsBundle\CmfSimpleCmsBundle(),
+            new Symfony\Cmf\Bundle\SeoBundle\CmfSeoBundle(),
 
             // dependency of the block bundle
             new Sonata\BlockBundle\SonataBlockBundle(),
+            new Sonata\SeoBundle\SonataSeoBundle(),
+
 
             // and the website specific bundle
             new Cmf\MainBundle\CmfMainBundle(),
