d4e88c3ee61076b4aa0d344127838b89de77d9f5
Lukas Kahwe Smith <smith@pooteeweet.org>
2015-04-03 18:13:57 +0200

    removed SecurityBundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -10,7 +10,6 @@ class AppKernel extends Kernel
         $bundles = array(
             // enable symfony-standard bundles
             new Symfony\Bundle\FrameworkBundle\FrameworkBundle(),
-            new Symfony\Bundle\SecurityBundle\SecurityBundle(),
             new Symfony\Bundle\TwigBundle\TwigBundle(),
             new Symfony\Bundle\MonologBundle\MonologBundle(),
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
